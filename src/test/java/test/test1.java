6560666dacfb079af122685fcfab22deee0467f7
Gladson Antony <GladsonAntony91@gmail.com>
2017-04-20 13:50:58 +0530

    Setup Video Recording

--- src/test/java/test/test1.java
+++ src/test/java/test/test1.java
@@ -13,28 +13,58 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
+import com.automation.remarks.testng.VideoListener;
+import com.automation.remarks.video.annotations.Video;
+import com.automation.remarks.video.enums.RecorderType;
+import com.automation.remarks.video.enums.RecordingMode;
+import com.automation.remarks.video.enums.VideoSaveMode;
+import com.automation.remarks.video.recorder.VideoRecorder;
+import com.automation.remarks.video.recorder.monte.MonteRecorder;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
 import io.github.bonigarcia.wdm.PhantomJsDriverManager;
 
+@Listeners(VideoListener.class)
 public class test1 
 {
 	public static WebDriver driver;
+
+	@BeforeMethod
+	public void beforeClass() throws Exception
+	{
 	
-	@Test
+		VideoRecorder.conf()         // Default is ${user.dir}/video.
+		.videoEnabled(true)                       	// Disabled video globally
+		.withVideoSaveMode(VideoSaveMode.ALL)     	// Save videos for passed and failed tests
+		.withRecorderType(RecorderType.FFMPEG)    // Monte is Default recorder
+		.withRecordMode(RecordingMode.ALL)  ;		// Record video only for tests with @Video
+		//.withScreenSize(1024,768);					// Set screen size
+/*		
+		MonteRecorder.conf()
+		.withVideoFolder("C:\\Gladson\\EclipseWorkspace\\WebAutomation_AllureParallel\\video")
+		.withRecordMode(RecordingMode.ALL)
+        .withRecorderType(RecorderType.MONTE);*/
+	}
+
+
+	@Test @Video(name="ChromeTest")
 	public void test_Chrome() throws Exception
 	{
 		ChromeDriverManager.getInstance().setup();
 		driver = new ChromeDriver();
 		driver.get("https://www.google.co.in/");
-		Assert.assertEquals(driver.getTitle(), "Google");
-		driver.close();
+		Assert.assertEquals(driver.getTitle(), "Boogle");
+		//driver.close();
 	}
-	
-	@Test
+
+	@Test @Video
 	public void test_Firefox() throws Exception
 	{
 		FirefoxDriverManager.getInstance().setup();
@@ -42,9 +72,9 @@ public class test1
 		driver.manage().window().maximize();
 		driver.get("https://www.google.co.in/");
 		Assert.assertEquals(driver.getTitle(), "Google");
-		driver.close();
+		//driver.close();
 	}
-	
+
 	@Test
 	public void test_Phanthom() throws Exception
 	{
@@ -57,10 +87,10 @@ public class test1
 		System.out.println(driver.getCurrentUrl());
 		System.out.println(driver.getTitle());
 		Assert.assertEquals(driver.getTitle(), "Google");
-		driver.close();
+		//driver.close();
 	}
 
-	@Test
+	@Test @Video()
 	public void test_IE() throws Exception
 	{
 		InternetExplorerDriverManager.getInstance().setup();
@@ -70,6 +100,14 @@ public class test1
 		driver.manage().window().maximize();
 		driver.get("https://www.google.co.in/");
 		Assert.assertEquals(driver.getTitle(), "Google");
+		//driver.close();
+	}
+	
+	
+	@AfterMethod
+	public void teardown() throws Exception
+	{
 		driver.close();
+		driver.quit();
 	}
 }
